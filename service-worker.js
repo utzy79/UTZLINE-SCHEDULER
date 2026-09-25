8f927ebb2ac01dd125ff73078a01a7959427ca04
utzy79 <45909092+utzy79@users.noreply.github.com>
2026-09-25 10:26:00 +0930

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -292,7 +292,7 @@
 // suite (run_all.sh) re-run clean, zero regressions. Does NOT touch
 // source.html, Install ITP, Manufacture ITP, or Projects in any way.)
 var ICON_VERSION = "v1";
-var CACHE_NAME = "utzline-scheduler-cache-v17";
+var CACHE_NAME = "utzline-scheduler-cache-v18";
 
 var PRECACHE_URLS = [
   "./",
